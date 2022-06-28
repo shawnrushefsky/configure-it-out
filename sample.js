4bbf1bb784a34e85e451f3882060acf6e995164f
Shawn Rushefsky <shawn.rushefsky@glgroup.com>
2022-06-28 14:56:03 -0400

    remove

--- sample.js
+++
@@ -1,9 +0,0 @@
-const { A } = process.env;
-let mood = process.env.B;
-mood = process.env.C;
-const { B, C, D } = process.env;
-const obj = { D: 1, E: 2, F: 3 };
-const { D: G, E, F } = obj;
-
-const { env } = process;
-const { JK, LM: NO } = env;
